bbd7b06f41c1a21f39b452c3d28cf411898835cf
hansnam1105 <hansnam123@gmail.com>
2018-11-12 00:51:48 +0900

    some change

--- Calendar/Calendar.cpp
+++ Calendar/Calendar.cpp
@@ -3,6 +3,108 @@
 
 using namespace std;
 
+class Cal
+{
+private:
+	int year;
+	int month;
+public:
+	Cal(int year, int month)
+	{
+		cout << "Year : ";
+		cin >> year;
+		cout << "Month : ";
+		cin >> month;
+
+		cout << "일\t월\t화\t수\t목\t금\t토" << endl;
+	}
+};
+
+class printCal : public Cal
+{
+private:
+	int leap_year;
+	int daycount = 0, first_date = 0;
+	int count;
+public:
+	printCal(int year, int month) : Cal(year, month)
+	{
+		int lastday[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+		daycount = (year - 1) * 365 + (year - 1) / 4 - (year - 1) / 100 + (year - 1) / 400; //윤년
+		for (int i = 0; i < month - 1; i++) {
+			if (i == 1) {
+				if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+					lastday[1] = 29;
+				else
+					lastday[1] = 28;
+			}
+			daycount += lastday[i];
+		}
+		first_date = daycount % 7;
+		for (int j = 0; j <= first_date; j++)
+		{
+			cout << "\t";
+			count++;
+		}
+		for (int k = 1; k <= lastday[month - 1]; k++)
+		{
+			if (count >= 7)
+			{
+				cout << "" << endl;
+				count = 0;
+			}
+			cout << k << "\t";
+			count++;
+		}
+		cout << "\n\n";
+	}
+};
+
+class Schedule
+{
+private:
+	string schedulename;
+	int year, month, day;
+	int hour, minute;
+	int select = 0;
+public:
+	Schedule()
+	{
+		cout << "1.달력 2. 스케줄 추가";
+		cin >> select;
+		if (select == 1)
+		{
+			 
+		}
+		else if (select == 2)
+		{
+			addSchedule();
+		}
+	}
+	void addSchedule()
+	{
+		cout << "스케줄 이름을 입력해주세요 : ";
+		cin >> schedulename;
+		cout << "일정 \n 년 : ";
+		cin >> year;
+		cout << "달 : ";
+		cin >> month;
+		cout << "일 : ";
+		cin >> day;
+		cout << "1. 하루종일\n2.시간설정" << endl;
+		cin >> select;
+		if (select == 2)
+		{
+			cout << "시간 : ";
+			cin >> hour;
+			cout << "분 : ";
+			cin >> minute;
+		}
+
+	}
+};
+
 class Signin
 {
 protected:
@@ -56,7 +158,7 @@ public:
 	{
 		if ((insertid == id) && (insertpin == pin))
 		{
-			
+			Schedule::Schedule();
 		}
 		else if((insertid == id) && (insertpin != pin))
 		{
@@ -67,96 +169,6 @@ public:
 			cout << "Wrong ID" << endl;
 		}
 		
-	}
-};
-
-class Schedule
-{
-private:
-	string schedulename;
-	int year, month, day;
-	int hour, minute;
-	int select;
-public:
-
-	void addSchedule()
-	{
-		cout << "스케줄 이름을 입력해주세요 : ";
-		cin >> schedulename;
-		cout << "일정 \n 년 : ";
-		cin >> year;
-		cout << "달 : ";
-		cin >> month;
-		cout << "일 : ";
-		cin >> day;
-		cout << "1. 하루종일\n2.시간설정" << endl;
-		cin >> select;
-		if (select == 2)
-		{
-			cout << "시간 : ";
-			cin >> hour;
-			cout << "분 : ";
-			cin >> minute;
-		}
-
-	}
-};
-
-class Cal
-{
-private:
-	int year;
-	int month;
-public:
-	Cal(int year, int month)
-	{
-		cout << "Year : ";
-		cin >> year;
-		cout << "Month : ";
-		cin >> month;
-
-		cout << "일\t월\t화\t수\t목\t금\t토" << endl;
-	}
-};
-
-class printCal : public Cal
-{
-private:
-	int leap_year;
-	int daycount = 0 , first_date=0;
-	int count;
-public:
-	printCal(int year, int month) : Cal(year, month)
-	{
-		int lastday[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
-		daycount = (year - 1) * 365 + (year - 1) / 4 - (year - 1) / 100 + (year - 1) / 400; //윤년
-		for (int i = 0; i < month - 1; i++) { 
-			if (i == 1) {
-				if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) 
-					lastday[1] = 29;
-				else
-					lastday[1] = 28;
-			}
-			daycount += lastday[i];
-		}
-		first_date = daycount % 7;
-		for (int j = 0; j <= first_date; j++)
-		{
-			cout << "\t";
-			count++;
-		}
-		for (int k = 1; k <= lastday[month - 1]; k++)
-		{
-			if (count >= 7)
-			{
-				cout << "" << endl;
-				count = 0;
-			}
-			cout << k << "\t";
-			count++;
-		}
-		cout << "\n\n";
 	}
 };
 
